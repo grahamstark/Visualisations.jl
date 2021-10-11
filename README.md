6dab9580ef5957cffbc7731d3bc0c8ce849e8100
Graham Stark <graham.stark@virtual-worlds.biz>
2021-10-11 15:31:00 +0100

    readme

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# Visualisations
+# Visualisations for the Scottish Tax and Benefit Model
+
+This is where interactive visualisations of the [Scottish Tax Benefit Model](https://github.com/grahamstark/ScottishTaxBenefitModel.jl) will live.
+
+This is a seperate repository so as to keep all the messy web stuff away from the model itself.
+
+I don't plan on there being a full blown web user interface for all the model's hundreds of parameters, as it's very boring to write those and no-one actually uses them - for detailed work something reproducable like [Dr. Watson]() is likely a better bet. Instead, I'm going to make a series of small, single screen, interfaces in [Pluto](https://plutojl.org/) and [Dash](https://dash-julia.plotly.com/) on particular topics.
+
+![BC Demo](https://github.com/grahamstark/ScottishTaxBenefitModel.jl/blob/master/docs/bc1.gif)
 
 [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://grahamstark.github.io/Visualisations.jl/stable)
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://grahamstark.github.io/Visualisations.jl/dev)
-[![Build Status](https://travis-ci.com/grahamstark/Visualisations.jl.svg?branch=master)](https://travis-ci.com/grahamstark/Visualisations.jl)
-[![Coverage](https://codecov.io/gh/grahamstark/Visualisations.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/grahamstark/Visualisations.jl)
--- src/Dash1.jl
+++ src/Dash1.jl
@@ -1,5 +1,7 @@
 
 using Dash
+# using DashHtmlComponents
+# using DashCoreComponents
 using PlotlyJS
 
 #, DashHtmlComponents, DashCoreComponents
@@ -14,8 +16,6 @@ using .ExampleHouseholdGetter
 using .STBParameters
 using Markdown
 using DataFrames
-
-app = dash()
 
 function getbc( 
 	hh  :: Household, 
@@ -33,6 +33,20 @@ function getbc(
     (lbc,ubc)
 end
 
+function get_hh( name :: String )
+	hh = get_household( name )
+	head = get_head(hh)
+	set_wage!( head, 0, 10 )
+	spouse = get_spouse( hh )
+	if spouse !== nothing
+		set_wage!( spouse, 0, 10 )
+	end
+	for (pid,pers) in hh.people
+		println( "age=$(pers.age) empstat=$(pers.employment_status) " )
+		empty!( pers.income )
+	end
+	return hh
+end
 
 function bcplot( lbc:: DataFrame, ubc :: DataFrame )
 	bl = scatter(
@@ -60,14 +74,15 @@ function bcplot( lbc:: DataFrame, ubc :: DataFrame )
 	gn["marker"] = Dict(:color => "#ccc",
                         :line => Dict(:color=> "#ccc",
                                          :width=> 0.5))
-	layout = Layout(title="Budget Constraint: Legacy Benefits vs Universal Credit",
+	layout = Layout(
+		title="Budget Constraint: Legacy Benefits vs Universal Credit",
         xaxis_title="Person's Gross Earnings £p.w.",
         yaxis_title="Household Net Income £p.w.",
 		xaxis_range=[0, 1_200],
 		yaxis_range=[0, 1_200],
-		
-		width=800, 
-		height=650)
+		legend=attr(x=0.01, y=0.95),
+		width=700, 
+		height=700)
 	p = PlotlyJS.plot( [gn, bl, bu], layout)
 	println(typeof(p))
 	p
@@ -97,51 +112,39 @@ push!(d, Dict("label"=>"Lone Parent, 2 Children", "value" => "single_parent_1"))
 push!(d, Dict("label"=>"Single Person", "value" => "mel_c2"))
 
 
-
-hh = get_household("example_hh1")
-head = get_head(hh)
-empty!( head.income )
-spouse = get_spouse( hh )
-if spouse !== nothing
-	empty!( spouse.income )
-end
-
-
-
+hh = get_hh( "example_hh1" )
 sys = load_file( joinpath( Definitions.MODEL_PARAMS_DIR, "sys_2021_22.jl" ))
 load_file!( sys, joinpath( Definitions.MODEL_PARAMS_DIR, "sys_2021-uplift-removed.jl"))
 # println( "weeklyise start wpm=$PWPM wpy=52")
 weeklyise!( sys )
 settings = Settings()
 lbc, ubc = getbc(hh,sys,settings)
+println( lbc.simplelabel )
 
 # app.layout = html_div() do
+
+app = dash()
 app.layout = html_div(style = Dict("columnCount" => 2)) do
 
-    html_h1("Budget Constraint Example"),
-    html_div(""),
-    
-	html_label("Family"),
-    dcc_dropdown(options = d, value = "example_hh1"),
-    # generate_table( lbc ),
-	dcc_graph(
-        id = "bc-1",
-		figure=bcplot( lbc, ubc )
-    ),
+    html_h1("Budget Constraint Example"),	
+	# html_div(style=Dict( "column_width"=>0.6 )),
+		html_h3( "col1")
+		html_label("Family"),
+		dcc_dropdown(options = d, value = "example_hh1"),
+		# generate_table( lbc ),
+		dcc_markdown(
+			"Created with [Julia](https://julialang.org/) | [Dash](https://dash-julia.plotly.com/) | [Plotly](https://plotly.com/julia/) | [Budget Constraint Generator](https://github.com/grahamstark/BudgetConstraints.jl)"),
+		dcc_markdown(
+			"Part of the [Scottish Tax Benefit Model](https://github.com/grahamstark/ScottishTaxBenefitModel.jl)."),
+		dcc_markdown(
+			"This is Open Source software released under the [MIT Licence](https://github.com/grahamstark/Visualisations.jl/blob/main/LICENSE). [Source Code](https://github.com/grahamstark/Visualisations.jl)",
+		),
+	# html_div( style=Dict( "column_width"=>0.39 )),
+		dcc_graph(
+			id = "bc-1",
+			figure=bcplot( lbc, ubc )
+		)
 	
-	dcc_markdown(
-	"Created with [Julia](https://julialang.org/) | 
-	[Dash](https://dash-julia.plotly.com/) | 
-	[Plotly](https://plotly.com/julia/) | 
-	[Budget Constraint Generator](https://github.com/grahamstark/BudgetConstraints.jl)"),
-	dcc_markdown(
-		"Part of the [Scottish Tax Benefit Model](https://github.com/grahamstark/ScottishTaxBenefitModel.jl).
-		"),
-	dcc_markdown(
-		"This is Open Source software released under the [MIT Licence](https://github.com/grahamstark/Visualisations.jl/blob/main/LICENSE).
-		[Source Code](https://github.com/grahamstark/Visualisations.jl)"
-	)
-
 end
 
 run_server(app, "0.0.0.0", debug=true)
