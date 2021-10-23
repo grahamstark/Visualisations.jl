441bab5b5842e3ce49c29865e356913d426eaa27
Graham Stark <graham.stark@virtual-worlds.biz>
2021-10-23 18:19:42 +0100

    text and labels

--- src/bc_demo.jl
+++ src/bc_demo.jl
@@ -21,20 +21,37 @@ const FORM_EXTRA =Dict(
 	"margin-bottom"=>"5px",
 	"margin-top"=>"5px")
 
+const PREAMBLE = """
+Regardless of politics, I imagine most people an agree on the outlines of what
+our taxation and benefit system should do. Give some support to people who haven't got much,
+(depending who you ask, maybe take that support away from better off people); as you
+get better off, pay a bit (or a lot) more in tax. But that's not always what the system(s) 
+we have do. Here's a simulation showing how the rules of the tax and benefit system 
+interact to make it almost impossible for households to make sensible choices about
+whether it's worth taking a job, working a bit more, or going for a promotion.  Using the
+controls to the left, you can see from the charts how the choices between what you earn and what
+you end up with vary for different family times (renting or owning, with and without children, high or low earners). 
+"""
+
 const INFO = """
-This illustrates how net the net income of a household (i.e. after deducting taxes and housing costs, then adding benefits)
-varies with gross earnings, under the new Universal Credit benefit system and the old
-'legacy' tax-credit system. 
+The graph shows how net the net income of a household (i.e. after deducting taxes and optionally housing costs, then adding benefits)
+varies with gross earnings, under the new [Universal Credit](https://www.gov.uk/universal-credit) 
+benefit system and the
+['legacy' Tax Credit system](https://www.gov.uk/topic/benefits-credits/tax-credits) which UC is gradually replacing. You can also switch to an 'economist's 
+view' of the labour/leisure choice. Move your mouse over the chart to see a detailed
+breakdown of what's happening at each point.
 
 For simplicity, we assume:
 
 * only one person in the household works;
-* there are no other sources of income;
-* the working person faces a fixed £10 hourly wage and can work any hours at that wage.
+* the adults are of working age;
+* there are no other sources of income, for example savings income or disability benefits; and
+* the working person faces a fixed hourly wage and can work any hours at that wage.
 
 You'll see that the graphs are complicated enough even with those simplifications.
+This is especially so for families with large housing costs
+or several children, for whom the [Benefit Cap](https://www.gov.uk/benefit-cap) bites.
 
-Choose one of the three example families above. Move your mouse over the 'kink points' in the graph for a detailed calculation.
 
 See [here](https://stb.virtual-worlds.scot/bc-intro.html) for more on the ideas behind budget constraints.
 
@@ -149,7 +166,7 @@ end
 """
 Plot two budget constraints (contained in dataframes) - legacy & universal credit.
 """
-function econ_bcplot( lbc:: DataFrame, ubc :: DataFrame, wage :: Real )
+function econ_bcplot( lbc:: DataFrame, ubc :: DataFrame, wage :: Real, ytitle :: String )
 	# legacy
 	gross_to_leisure!(lbc, wage )
 	bl = scatter(
@@ -183,7 +200,7 @@ function econ_bcplot( lbc:: DataFrame, ubc :: DataFrame, wage :: Real )
 	layout = Layout(
 		title="Budget Constraint: Legacy Benefits vs Universal Credit",
         xaxis_title="Leisure (hours p.w.)",
-        yaxis_title="Household Net Income After Housing Costs £p.w.",
+        yaxis_title=ytitle,
 		xaxis_range=[0, MAX_HRS ],
 		yaxis_range=[0, 1_500],
 		legend=attr(x=0.01, y=0.95),
@@ -198,7 +215,7 @@ end
 """
 Plot two budget constraints (contained in dataframes) - legacy & universal credit.
 """
-function bcplot( lbc:: DataFrame, ubc :: DataFrame )
+function bcplot( lbc:: DataFrame, ubc :: DataFrame, ytitle :: String )
 	# legacy
 	bl = scatter(
            lbc, 
@@ -227,8 +244,8 @@ function bcplot( lbc:: DataFrame, ubc :: DataFrame )
                         :width=> 0.5))
 	layout = Layout(
 		title="Budget Constraint: Legacy Benefits vs Universal Credit",
-        xaxis_title="Person's Gross Earnings £p.w.",
-        yaxis_title="Household Net Income After Housing Costs £p.w.",
+        xaxis_title="Household Gross Earnings £p.w.",
+        yaxis_title=ytitle,
 		xaxis_range=[0, 1_200],
 		yaxis_range=[0, 1_200],
 		legend=attr(x=0.01, y=0.95),
@@ -255,20 +272,26 @@ function doplot(
 	hh = get_hh( tenure, bedrooms, hcost, marrstat, chu5, ch5p )
 	# println(to_md_table(hh))
 	settings = Settings()
-	target = if target_str == "ahc_hh" 
-		 ahc_hh 
-		elseif target_str == "bhc_hh"
-			bhc_hh 
-		elseif target_str == "total_bens" 
-			total_bens 
-		elseif target_str == "total_taxes"
-			total_taxes 
-		end # forgot how to covert
+	ytitle = ""
+	target = nothing
+	if target_str == "ahc_hh" 
+		target = ahc_hh 
+		ytitle = "Household Net Income After Housing Costs £p.w."
+	elseif target_str == "bhc_hh"
+		target = bhc_hh 
+		ytitle = "Household Net Income Before Housing Costs £p.w."
+	elseif target_str == "total_bens" 
+		ytitle = "Total State Benefits received £pw"
+		target = total_bens 
+	elseif target_str == "total_taxes"
+		ytitle = "Total Income Tax and NI Payments £pw"
+		target = total_taxes 
+	end # forgot how to covert
 	lbc, ubc = getbc( hh, sys, wage, settings, target )
 	if view == "l_vs_l"
-		figure=econ_bcplot( lbc, ubc, wage )
+		figure=econ_bcplot( lbc, ubc, wage, ytitle )
 	else
-		figure = bcplot( lbc, ubc )
+		figure = bcplot( lbc, ubc, ytitle )
 	end
 	return figure 
 end
@@ -440,12 +463,14 @@ sys = load_file( joinpath( Definitions.MODEL_PARAMS_DIR, "sys_2021_22.jl" ))
 load_file!( sys, joinpath( Definitions.MODEL_PARAMS_DIR, "sys_2021-uplift-removed.jl"))
 weeklyise!( sys )
 
-app = dash(external_stylesheets=[dbc_themes.UNITED]) 
-# BOOTSTRAP|SIMPLEX|MINTY|COSMO|SANDSTONE|UNITED|SLATE|SOLAR
+app = dash(external_stylesheets=[dbc_themes.SANDSTONE]) 
+# BOOTSTRAP|SIMPLEX|MINTY|COSMO|SANDSTONE|UNITED|SLATE|SOLAR|UNITED|
 app.layout = dbc_container(fluid=true, className="p-5") do
 	
-	html_h1("Household Budget Constraints: Legacy vs UC Examples"),
-
+	html_h1("Scotland's Kinkiest Families: Household Budget Constraints"),
+	dbc_row([
+		dbc_col( dcc_markdown( PREAMBLE ), width=10)
+	]),
 	dbc_row([
     	dbc_col(get_input_block(), width=4),
 		dbc_col( dcc_graph( id = "bc-1" ))
