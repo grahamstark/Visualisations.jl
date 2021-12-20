fd1ab2817f102a0adbcf3f2a617cbcf52145ef81
Graham Stark <graham.stark@virtual-worlds.biz>
2021-12-20 01:20:35 +0000

    project

--- src/server.jl
+++ src/server.jl
@@ -1,7 +1,7 @@
 
 serve(stbdemo, port)
 
-include( "server_libs.jl")
+include( "src/server_libs.jl")
 
 while true # FIXME better way?
    @debug "new STB Server; main loop; server running on port $port"
