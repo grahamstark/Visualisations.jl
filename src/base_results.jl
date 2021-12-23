310f964769b358ade50d45fd629543c88b04a602
Graham Stark <graham.stark@virtual-worlds.biz>
2021-12-23 01:12:06 +0000

    zx

--- src/base_results.jl
+++ src/base_results.jl
@@ -4,7 +4,7 @@
 const BASE_UUID = UUID("985c312f-129b-4acd-9e40-cb629d184183")
 const BASE_SETTINGS = initialise_settings()
 const BASE_PARAMS = load_system()
-const BASE_OUTPUT = do_run_a( "default", BASE_PARAMS, BASE_PARAMS, BASE_SETTINGS )
+const BASE_RESULTS = do_run_a( "default", BASE_PARAMS, BASE_SETTINGS )
 const BASE_TEXT_OUTPUT = results_to_html( BASE_UUID, BASE_OUTPUT, BASE_OUTPUT )
 
 
